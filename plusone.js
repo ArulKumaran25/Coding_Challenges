a36183a74e75e8384f84880d73837effae06170d
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-10-15 17:50:37 +0530

    Coding challenges updated

---
+++ plusone.js
@@ -0,0 +1,13 @@
+function plusone(digits) {
+    for (var i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1);
+    return digits;
+}
+var examplee = [1, 2, 3];
+console.log(plusone(examplee));
