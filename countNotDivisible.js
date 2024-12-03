35efb03d521994779955d9a6c3921926f820d09d
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-12-03 17:31:21 +0530

    coding challenges added

---
+++ countNotDivisible.js
@@ -0,0 +1,24 @@
+// You have to print the count of numbers which are not divided by n within the given range.
+// Input:
+// start = 5;
+// end = 12;
+// n = 5;
+// output:
+// 7
+// Explanation:
+// 6,7,8,9,10,11,12 are not divided by 5. So, its count is 7
+function countNotDivisibleByN(start, end, n) {
+    var count = 0;
+    for (var i = start; i <= end; i++) {
+        if (i % n !== 0) {
+            count++;
+        }
+    }
+    return count;
+}
+// Example usage:
+var start = 5;
+var end = 12;
+var n = 5;
+var result = countNotDivisibleByN(start, end, n);
+console.log(result); // Output: 7
---
+++ countNotDivisible.ts
@@ -0,0 +1,30 @@
+// You have to print the count of numbers which are not divided by n within the given range.
+
+// Input:
+// start = 5;
+// end = 12;
+// n = 5;
+// output:
+// 7
+
+// Explanation:
+// 6,7,8,9,10,11,12 are not divided by 5. So, its count is 7
+
+
+function countNotDivisibleByN(start: number, end: number, n: number): number {
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        if (i % n !== 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Example usage:
+
+const start = 5;
+const end = 12;
+const n = 5;
+const result = countNotDivisibleByN(start, end, n);
+console.log(result); // Output: 7
