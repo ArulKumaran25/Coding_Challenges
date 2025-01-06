cec9ca0af81249fffd666e976a2c97ef4bb79082
Arul Kumaran25 <arulkumaran2519@gmail.com>
2025-01-06 15:08:10 +0530

    updations in longest Palindrome

--- longestPalindrome.ts
+++ longestPalindrome.ts
@@ -2,8 +2,8 @@ function longestPalindromicSubstring(s:string):string{
     if(s.length===0)
         return "";
     let longest="";
-    const expandAroundCenter=(left: number,right:number)=>{
-        while (left>=0 && right<s.length && s[left] === s[right]) {
+    const expandAroundCenter=(left:number,right:number)=>{
+        while (left>=0 && right<s.length && s[left]===s[right]) {
             const currentPalindrome=s.slice(left, right + 1);
             if(currentPalindrome.length>longest.length) {
                 longest=currentPalindrome;
@@ -12,12 +12,10 @@ function longestPalindromicSubstring(s:string):string{
             right++;
         }
     };
-
-    for (let i = 0; i < s.length; i++) {
-        expandAroundCenter(i, i);
-        expandAroundCenter(i, i + 1);
+    for (let i=0;i<s.length;i++) {
+        expandAroundCenter(i,i);
+        expandAroundCenter(i,i + 1);
     }
-
     return longest;
 }
 console.log(longestPalindromicSubstring("babad"));      // Output: "bab" or "aba"
